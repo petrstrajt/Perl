0230125d6868d719691ae333cac732104c9b4a25
Petr Štrajt <48010844+petrstrajt@users.noreply.github.com>
2023-02-02 20:10:11 +0100

    Add files via upload

---
+++ if.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+
+print "Zadej cislo vetsi nez nula \n";
+my $cislo = <STDIN>;
+
+if ($cislo > 0){
+    print "Takhle je to spravne \n";
+}
+
+else {
+    print "Zadani znelo, ze cislo ma byt vetsi nez nula \n";
+}
+
+
+
+
+    
+
+    
+    
